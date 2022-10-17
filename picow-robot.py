1b551450105a4184bc340210d76930d5fd79e818
ComputadorasySensores <79330985+ComputadorasySensores@users.noreply.github.com>
2022-10-17 09:15:58 -0300

    Create picow-robot.py
    Código en MicroPython correspondiente al Capítulo75 del canal de Youtube de Computadoras y Sensores. Aprovechando las funcionalidades de WiFi de la Raspberry Pi Pico controlamos un carrito, rover o robot.

---
+++ picow-robot.py
@@ -0,0 +1,126 @@
+import network
+import socket
+from time import sleep
+import machine
+from machine import Pin
+
+ssid = 'Ingresa el nombre de tu red WiFi'
+password = 'Ingresa contraseña de tu red WiFi'
+
+Motor_A_Adelante = Pin(18, Pin.OUT)
+Motor_A_Atras = Pin(19, Pin.OUT)
+Motor_B_Adelante = Pin(20, Pin.OUT)
+Motor_B_Atras = Pin(21, Pin.OUT)
+
+def adelante():
+    Motor_A_Adelante.value(1)
+    Motor_B_Adelante.value(1)
+    Motor_A_Atras.value(0)
+    Motor_B_Atras.value(0)
+    
+def atras():
+    Motor_A_Adelante.value(0)
+    Motor_B_Adelante.value(0)
+    Motor_A_Atras.value(1)
+    Motor_B_Atras.value(1)
+
+def detener():
+    Motor_A_Adelante.value(0)
+    Motor_B_Adelante.value(0)
+    Motor_A_Atras.value(0)
+    Motor_B_Atras.value(0)
+
+def izquierda():
+    Motor_A_Adelante.value(1)
+    Motor_B_Adelante.value(0)
+    Motor_A_Atras.value(0)
+    Motor_B_Atras.value(1)
+
+def derecha():
+    Motor_A_Adelante.value(0)
+    Motor_B_Adelante.value(1)
+    Motor_A_Atras.value(1)
+    Motor_B_Atras.value(0)
+
+
+detener()
+    
+def conectar():
+    red = network.WLAN(network.STA_IF)
+    red.active(True)
+    red.connect(ssid, password)
+    while red.isconnected() == False:
+        print('Conectando ...')
+        sleep(1)
+    ip = red.ifconfig()[0]
+    print(f'Conectado con IP: {ip}')
+    return ip
+    
+def open_socket(ip):
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    return connection
+
+def pagina_web():
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+            <head>
+            </head>
+            <body>
+            <center>
+            <form action="./adelante">
+            <input type="submit" value="Adelante" style="background-color: #04AA6D; border-radius: 15px; height:120px; width:120px; border: none; color: white; padding: 16px 24px; margin: 4px 2px"  />
+            </form>
+            <table><tr>
+            <td><form action="./izquierda">
+            <input type="submit" value="Izquierda" style="background-color: #04AA6D; border-radius: 15px; height:120px; width:120px; border: none; color: white; padding: 16px 24px; margin: 4px 2px"/>
+            </form></td>
+            <td><form action="./detener">
+            <input type="submit" value="Detener" style="background-color: #FF0000; border-radius: 50px; height:120px; width:120px; border: none; color: white; padding: 16px 24px; margin: 4px 2px" />
+            </form></td>
+            <td><form action="./derecha">
+            <input type="submit" value="Derecha" style="background-color: #04AA6D; border-radius: 15px; height:120px; width:120px; border: none; color: white; padding: 16px 24px; margin: 4px 2px"/>
+            </form></td>
+            </tr></table>
+            <form action="./atras">
+            <input type="submit" value="Atras" style="background-color: #04AA6D; border-radius: 15px; height:120px; width:120px; border: none; color: white; padding: 16px 24px; margin: 4px 2px"/>
+            </form>
+            </body>
+            </html>
+            """
+    return str(html)
+
+def serve(connection):
+    while True:
+        cliente = connection.accept()[0]
+        peticion = cliente.recv(1024)
+        peticion = str(peticion)
+        try:
+            peticion = peticion.split()[1]
+        except IndexError:
+            pass
+        if peticion == '/adelante?':
+            adelante()
+        elif peticion =='/izquierda?':
+            izquierda()
+        elif peticion =='/detener?':
+            detener()
+        elif peticion =='/derecha?':
+            derecha()
+        elif peticion =='/atras?':
+            atras()
+        html = pagina_web()
+        cliente.send(html)
+        cliente.close()
+
+try:
+    ip = conectar()
+    connection = open_socket(ip)
+    serve(connection)
+except KeyboardInterrupt:
+    machine.reset()
+
+    
